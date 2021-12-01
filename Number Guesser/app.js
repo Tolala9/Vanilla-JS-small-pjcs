9162168ad343719af744ac429d44f20e5ba5b34d
Tolala9 <anatolii.lukavenko@gmail.com>
2021-12-01 17:42:22 +0200

    Added win functionality to guess game

--- Number Guesser/app.js
+++ Number Guesser/app.js
@@ -1,14 +1,14 @@
 let min = 1,
-    max = 10,
-    winningNum = 2,
-    guessesLeft = 3;
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
 
 const game = document.getElementById('game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessBtn = document.querySelector('#guess-btn'),
-      guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message');
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessBtn = document.querySelector('#guess-btn'),
+  guessInput = document.querySelector('#guess-input'),
+  message = document.querySelector('.message');
 
 minNum.textContent = min;
 maxNum.textContent = max;
@@ -21,11 +21,20 @@ guessBtn.addEventListener('click', function () {
   }
 
   if (guess === winningNum) {
-    guessInput.disabled = true;
-    guessInput.style.borderColor = 'green';
-    setMessage(`${winningNum} is correct! YOU WIN!`, 'green')
+    gameOver(true, `${winningNum} is correct! YOU WIN!`);
 
   } else {
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
+
+    } else {
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`)
+      guessInput.style.borderColor = 'red';
+      guessInput.value = '';
+
+    }
 
   }
 });
@@ -33,4 +42,13 @@ guessBtn.addEventListener('click', function () {
 function setMessage(msg, color) {
   message.textContent = msg;
   message.style.color = color
+}
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? color = "green" : "red";
+
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg, color)
 }
